99fa5bbb7baab38ae5a21f7a1d94d02759bb4d42
game110pm <game110pm@605-110-STU06>
2017-02-01 13:21:16 -0700

    england

---
+++ question/question/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char input;
+	cout << "Do you like the new england patriots?" << endl;
+	cin >> input;
+	if (input == 'y')
+		cout << "There a cool team" << endl;
+	else if (input == 'n')
+		cout << " Get the f**k out here" << endl;
+}
