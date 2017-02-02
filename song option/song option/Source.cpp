c12bd89d5784ca6c3e863178f6d21376a4a5e32e
game110pm <game110pm@605-110-STU06>
2017-02-02 13:09:18 -0700

    song options

---
+++ song option/song option/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char input;
+	cout << "What song would you want to play?" << endl;
+	cin >> input;
+	if (input == 1)
+		cout << "City's breaking down on a camel's back." << endl;
+	else if (input == 2) {
+		cout << "Do you know the enemy" << endl;
+		cout << "Do you know your enemy" << endl;
+		cout << "Well gotta' know the enemy, ohaooh!" << endl;
+	}
+	else if (input == 3)
+	cout << "No sleep 'til Brooklyn" << endl;
+	cout << "Foot on the pedal never ever false metal" << endl;
+	cout << "Engine running hotter than a boiling kettle" << endl;
+
+
+}
