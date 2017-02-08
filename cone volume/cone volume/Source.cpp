3df3e161131a8c5283fefc4466ed0ed2f8a2747f
game110pm <game110pm@605-110-STU06>
2017-02-08 12:31:30 -0700

    blah

---
+++ cone volume/cone volume/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double Height;
+	double radius;
+	double Volume;
+	cout << "what is your height" << endl;
+	cin >> Height;
+	cout << "what is your radius" << endl;
+	cin >> radius;
+	Volume = Height*radius / 3;
+	cout << "your volume will be " << Volume << endl;
+
+
+}
