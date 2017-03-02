7fb40ff5c31231514e34e0d6d736e6c4c669b6ad
game110pm <game110pm@605-110-STU06>
2017-03-02 13:15:15 -0700

    davion

---
+++ partner burp/partner burp/Source.cpp
@@ -0,0 +1,16 @@
+#include <Windows.h>
+#include<iostream>
+using namespace std;
+void burp();// decleration
+int main() {
+	burp();//call
+
+}
+void burp() {//definition
+	for (int i = 0; i = i < 5 * 2; i++)
+		PlaySound(TEXT("voice_of_someone.wav"), NULL, SND_FILENAME);
+
+
+
+
+}
